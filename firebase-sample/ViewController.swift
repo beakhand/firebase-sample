011791663d6149a5699f4c1f3441b398620a6cf7
beakhand <cat51_2@hotmail.co.jp>
2020-05-25 23:31:03 +0900

    initial commit

---
+++ firebase-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-sample
+//
+//  Created by beakhand on 2020/05/25.
+//  Copyright © 2020 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
